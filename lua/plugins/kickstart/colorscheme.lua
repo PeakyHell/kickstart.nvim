49bb7df4ef623c21cc838b31c7a0dba9aab37380
PeakyHell <contact@peakyhell.com>
2025-12-12 12:15:23 +0100

    Add catpuccin as a colorscheme

--- lua/plugins/kickstart/colorscheme.lua
+++ lua/plugins/kickstart/colorscheme.lua
@@ -1,19 +1,33 @@
 -- PERF: Colorscheme
 return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
-      },
-    }
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
 
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-  end,
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  {
+    'catppuccin/nvim',
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('catppuccin').setup {
+        styles = {
+          comments = {}, -- Disable italics in comments
+        },
+      }
+
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
+  },
 }
